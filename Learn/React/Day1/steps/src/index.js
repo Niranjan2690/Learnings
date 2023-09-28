6c92e961ddd6cfa28a9b518f8b01933cbc739668
Niranjan2690 <110157126+Niranjan2690@users.noreply.github.com>
2023-09-28 12:39:03 +0530

    Add files via upload

--- Learn/React/Day1/steps/src/index.js
+++ Learn/React/Day1/steps/src/index.js
@@ -1,6 +1,21 @@
 import React, {Component} from "react";
 import ReactDOM from "react-dom/client";
-import Frag from "./fragment"
+//import Frag from "./fragment";
+import AppComponent from "./class-based-components/app";
+import ParentComponent from "./state/parent";
+import ComponentCommunication from "./component-communication/communication";
+import ComponentLifecycle from "./component-lifecycle/componentlifecycle";
+/* import HigherOrder from './higher-order-component/higher-order'; */
+import UseState from "./hooks/useState";
+import UseRef from "./hooks/useRef";
+import UseEffect from "./hooks/useeffect";
+import UseEffectAjax from "./hooks/useeffectajax";
+import Main from "./context_API/main";
+import FunMain from "./context_API_using_Function/funmain";
+import UseReducer from "./hooks/useReducer";
+import ClassBasedForm from "./forms/class_based_form";
+import FunctionBasedForm from "./forms/function_based_form";
+import UseReducerForm from "./forms/useReducer_form";
 
 
 let Elm = function(){
@@ -18,5 +33,21 @@ class ClassComp extends Component{
 
 ReactDOM.createRoot(document.getElementsByClassName("root")[0])
 .render(<div>
-            <Elm /> <ClassComp /> <Frag />
+            <Elm /> 
+            <ClassComp /> 
+            <AppComponent />
+            <ParentComponent />
+            <ComponentCommunication />
+            <ComponentLifecycle />
+            {/* <HigherOrder /> */}
+            <UseState />
+            <UseRef/>
+            <UseEffect/>
+            <UseEffectAjax/>
+            <UseReducer/>
+            <Main/>
+            <FunMain/>
+            <ClassBasedForm/>
+            <FunctionBasedForm/>
+            <UseReducerForm/>
         </div>)
